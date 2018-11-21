f5821ed0ee019882c5d437a0817abc1840af3c67
gonzalobd <gonzalobd@outlook.com>
2018-11-21 14:23:11 +0100

    mapping port of web ui

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 docker rmi hadoop_local
 
-docker build -t hadoop_local . && docker run -it -p 9000:9000 hadoop_local
+docker build -t hadoop_local . && docker run -it -p 50070:50070 -p 9000:9000 hadoop_local
 
